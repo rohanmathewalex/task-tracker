f95c41029b192d0b3c4df71a1105c8808bcbd0b8
rohanmathewalex <rohanmathewalex6674@gmail.com>
2022-11-18 00:07:57 -0500

    state to delete

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 const Button = ({color,text,onClick}) => {
 
     return(
-        <button onClick={onClick} style={{backgroundColor:color}}>{text}</button>
+        <button className='btn' onClick={onClick}   style={{backgroundColor:color}}>{text}</button>
     )
 }
 Button.defaultProps = {
