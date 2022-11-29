dc3ab6afc4bd99511dc17c35305574741c8d5acd
rohanmathewalex <rohanmathewalex6674@gmail.com>
2022-11-29 01:50:06 -0500

    j-son backend server, fetch data from backend

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -3,9 +3,9 @@ const Tasks = ({tasks, onDelete,onToggle}) => {
     
   return (
      <>
-     {tasks.map((task) =>( 
+     {tasks.map((task, index) =>( 
      <Task
-      key={task.id}
+      key={index}
       task={task} 
       onDelete={onDelete}
       onToggle={onToggle}
